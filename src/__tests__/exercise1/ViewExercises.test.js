e9a799a5bd1467896c92d55d5cfe49e97cb6e955
Dhanushka <dhanushkamcr@gmail.com>
2025-11-10 17:09:41 -0600

    Write integration test for deleting an exercise

--- src/__tests__/exercise1/ViewExercises.test.js
+++ src/__tests__/exercise1/ViewExercises.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import ViewExercises from "../../exercise1/ViewExercises";
+import DeleteExercise from "../../exercise1/DeleteExercise";
+import { deleteExercise } from "../../exercise1/exercisesSlice";
 
 const mockStore = configureStore([]);
 
@@ -29,5 +31,24 @@ describe('ViewExercises component', () => {
 
         expect(getByText(/Running - 30 minutes - 300 kcal/i)).toBeInTheDocument();
         expect(getByText(/Cycling - 45 minutes - 450 kcal/i)).toBeInTheDocument();
+    });
+
+    describe('Delete exercise component', () => {
+        test('delete the exercise correctly', () => {
+            store.dispatch = jest.fn();
+
+            const { getByText } = render (
+                <Provider store={store}>
+                    <DeleteExercise id={'1'}/>
+                </Provider>
+            );
+
+            // Since redux-mock-store doesn't update the state, you won't be able to verify the UI change directly.
+            // Instead, verify the action was dispatched as expected.
+            // For UI changes, we should integrate an actual Redux store in our test or manually adjust the mocked store state.
+
+            fireEvent.click(getByText(/delete/i));
+            expect(store.dispatch).toHaveBeenCalledWith(deleteExercise('1'));
+        });
     });
 });
